78d8482fb6381063c7657e76330239f06d13bec9
Rafael de Sá <rafael_jdsb@hotmail.com>
2018-01-16 14:04:09 -0200

    Primeiro

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts._home')
+@section('content')
+
+    <h1>Index Page</h1>
+
+@endsection
---
+++ resources/views/layouts/_nav.blade.php
@@ -0,0 +1,20 @@
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+    <div class="container"> 
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Início</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Sobre</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contato</a>
+                </li>
+            </ul>
+        </div>
+    </div>    
+</nav>
